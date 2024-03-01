619f4f283f12752ff624de8ee8e1e6831de14ddf
oluceps <i@oluceps.uk>
2024-03-01 18:30:34 +0800

    ~ rebeauti

--- hosoer/src/App.tsx
+++ hosoer/src/App.tsx
@@ -1,5 +1,5 @@
 import type { Component } from "solid-js";
-import { Show, createEffect, createSignal, lazy } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 import { getHeartbeat } from "./apis/index";
 
 const App: Component = () => {
@@ -13,36 +13,36 @@ const App: Component = () => {
 
 	return (
 		<div class="w-screen h-screen overflow-hidden">
-			<div class="flex-row justify-end items-center h-auto bg-cerise-red-50" />
-			<div class="bg-cerise-red-50 backdrop-filter-none h-full w-full grid grid-rows-7 md:grid-rows-8 grid-cols-3 md:grid-cols-5">
-				<div class="flex overflow-auto justify-end items-center shadow-lg mx-4 md:row-start-3 md:row-span-3 md:col-start-2 md:col-span-3 row-start-3 row-span-3 col-start-1 col-span-3 bg-cerise-red-100 rounded-xl">
-					<div class="animate-pulse w-full flex justify-start items-center ml-11">
-						<div>
-							{/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="icon icon-tabler icon-tabler-activity-heartbeat"
-								width="100"
-								height="100"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="#b21e4b"
-								fill="none"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							>
-								<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-								<path d="M3 12h4.5l1.5 -6l4 12l2 -9l1.5 3h4.5" />
-							</svg>
+			<div class="flex-col justify-start h-full w-full bg-cerise-red-50 overflow-hidden">
+				<div class="flex-row justify-end items-center h-8" />
+				<div class="grid grid-cols-1 grid-rows-9 sm:grid-cols-9 sm:grid-rows-8 w-full h-full">
+					<div class="shadow-xl col-start-1 col-span-1 row-start-3 row-span-3 mx-4 sm:col-start-4 sm:col-span-3 sm:row-start-3 sm:row-span-3 bg-cerise-red-300 rounded-xl">
+						<div class="flex justify-evenly content-center items-center h-full w-full">
+							<div class="animate-pulse items-center">
+								{/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="icon icon-tabler icon-tabler-heartbeat"
+									width="100"
+									height="100"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="#b21e4b"
+									fill="none"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+									<path d="M19.5 13.572l-7.5 7.428l-2.896 -2.868m-6.117 -8.104a5 5 0 0 1 9.013 -3.022a5 5 0 1 1 7.5 6.572" />
+									<path d="M3 13h2l2 3l2 -6l1 3h3" />
+								</svg>
+							</div>
+
+							<span class="shadow-md font-mono text-6xl text-cerise-red-100 bg-cerise-red-200/20 backdrop-blur-lg px-3 pt-3 rounded-md">
+								<div class="">{heartbeat()}</div>
+							</span>
 						</div>
 					</div>
-
-					<span class="mr-3 shadow-md font-mono text-6xl text-cerise-red-50 bg-cerise-red-400 px-6 pb-3 pt-5 rounded-md">
-						<div class="">{heartbeat()}</div>
-					</span>
-					<span class="font-mono text-md text-neutral-400 bg-cerise-red-100 mr-4 mt-16">
-						bpm
-					</span>
 				</div>
 			</div>
 		</div>

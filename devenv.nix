09e188220f989d371aeaeff2a82cb511f8739bbf
oluceps <secinia@outlook.com>
2026-02-15 08:16:35 +0800

    +

---
+++ devenv.nix
@@ -0,0 +1,44 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+{
+
+  # overlays = [ inputs.rust-overlay.overlays.default ];
+
+  # cachix.enable = true;
+  # cachix.pull = [ "wrangler" ];
+  dotenv.enable = true;
+  languages.javascript = {
+    enable = true;
+    npm.install.enable = false;
+    pnpm.install.enable = false;
+  };
+
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+      "miri"
+    ];
+  };
+  # cachix.enable = true;
+  # cachix.pull = [ "wrangler" ];
+  packages = with pkgs; [
+    nodePackages_latest.pnpm
+    typescript
+    biome
+    rustup
+    wasm-pack
+    # (rust-bin.nightly.latest.default.override { extensions = [ "rust-src" ]; })
+    gemini-cli
+    # netlify-cli
+  ];
+  # ++ [ inputs.wrangler.packages.${pkgs.system}.default ];
+}
